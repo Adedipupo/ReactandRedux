e07935ac2f38ff8f0631b7a35fb48c959b3d192e
Adedipupo <bayonleolaluwa@gmail.com>
2020-04-24 02:28:33 +0100

    new code

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      title: ""
+    }
+  }
   render() {
     return <h2>Courses</h2>;
   }
