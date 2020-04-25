d5078bfbd27c7410f1ec191f70870a131516d645
Adedipupo <bayonleolaluwa@gmail.com>
2020-04-25 02:48:22 +0100

    new files

--- src/redux/reducers/courseReducers.js
+++ src/redux/reducers/courseReducers.js
@@ -1,5 +1,6 @@
 export default function courseReducers(state = [],action) {
     switch(action.type) {
-        
+      case: "CREATE_COURSE":
+       return [...state , {...action.course}];  
     }
 }
